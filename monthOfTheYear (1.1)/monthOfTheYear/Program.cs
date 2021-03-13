3c78c26fa17cd63af0996d3aa7c5728d12dfb2f2
Kohta1o <kurottu.kohtalo@gmail.com>
2021-03-13 21:02:34 +0700

    homework#1

---
+++ monthOfTheYear (1.1)/monthOfTheYear/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace monthOfTheYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int numMonth;
+                Console.WriteLine("Введите номер месяца: ");
+                numMonth = int.Parse(Console.ReadLine());
+                switch (numMonth)
+                {
+                    case 1:
+                        Console.WriteLine("Январь");
+                        break;
+                    case 2:
+                        Console.WriteLine("Февраль");
+                        break;
+                    case 3:
+                        Console.WriteLine("Март");
+                        break;
+                    case 4:
+                        Console.WriteLine("Апрель");
+                        break;
+                    case 5:
+                        Console.WriteLine("Май");
+                        break;
+                    case 6:
+                        Console.WriteLine("Июнь");
+                        break;
+                    case 7:
+                        Console.WriteLine("Июль");
+                        break;
+                    case 8:
+                        Console.WriteLine("Август");
+                        break;
+                    case 9:
+                        Console.WriteLine("Сентябрь");
+                        break;
+                    case 10:
+                        Console.WriteLine("Октябрь");
+                        break;
+                    case 11:
+                        Console.WriteLine("Ноябрь");
+                        break;
+                    case 12:
+                        Console.WriteLine("Декабрь");
+                        break;
+                    default:
+                        Console.WriteLine("Что это?");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ roseOfWind (1.2)/roseOfWind/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace roseOfWind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0, rg = 1;
+            char choice;
+            while (rg <= 10)
+            {
+                Console.WriteLine("Введите направление (С,Ю,З,В): ");
+                choice = char.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 'С':
+                    case 'с':
+                        y++;
+                        Console.WriteLine($"Сделано {rg} шага (Север)");
+                        rg += 1;
+                        break;
+                    case 'Ю':
+                    case 'ю':
+                        y--;
+                        Console.WriteLine($"Сделано {rg} шага (Юг)");
+                        rg += 1;
+                        break;
+                    case 'З':
+                    case 'з':
+                        x--;
+                        Console.WriteLine($"Сделано {rg} шага (Запад)");
+                        rg += 1;
+                        break;
+                    case 'В':
+                    case 'в':
+                        x++;
+                        Console.WriteLine($"Сделано {rg} шага (Восток)");
+                        rg += 1;
+                        break;
+                    default:
+                        Console.WriteLine("Что это?");
+                        break;
+                }
+            }
+            double result = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            Console.WriteLine($"Пройдено {result} м");
+            Console.ReadKey();
+        }
+    }
+}
